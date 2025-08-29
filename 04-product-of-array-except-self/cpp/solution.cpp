397df6f2ce81497d06ccc205f261263dad3482b2
Syn <seth.lunas01@gmail.com>
2025-08-28 23:03:01 -0700

    chore(solution.cpp): qualify size_t and include <cstddef> for portability with g++

--- 04-product-of-array-except-self/cpp/solution.cpp
+++ 04-product-of-array-except-self/cpp/solution.cpp
@@ -1,7 +1,9 @@
 #include "solution.hpp"
+#include <cstddef>
 
 std::vector<int> product_except_self(const std::vector<int>& nums) {
     using std::vector;
+    using std::size_t;
     size_t n = nums.size();
     vector<int> answer(n, 1); // Initialize vector 'answer' with the size n and all its values as 1
     
